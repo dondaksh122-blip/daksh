6130c4ac02c6555a69377ba31b96aca452eda2a5
dondaksh122-blip <dondaksh122@gmail.com>
2026-03-23 23:17:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ import requests
 
 app = Flask(__name__)
 
-API_URL = "http://87.120.165.68:17000/"
-API_KEY = "f0c649e78aa843ff"
+API_URL = "https://ansh-apis.is-dev.org/api/numinfofree"
+API_KEY = "anurag"
 
 
 @app.route("/")
 def home():
-    return "API Running"
+    return "Running"
 
 
 @app.route("/number")
@@ -18,35 +18,36 @@ def number():
     num = request.args.get("num")
 
     if not num:
-        return "Number missing"
+        return "❌ Number missing"
 
     params = {
-        "api_key": API_KEY,
-        "NumberInfo": num
+        "key": API_KEY,
+        "num": num
     }
 
-    r = requests.get(API_URL, params=params)
-    data = r.json()
+    try:
+        r = requests.get(API_URL, params=params, timeout=20)
+        data = r.json()
+    except:
+        return "❌ API Error"
 
-    if data["status"] != "success":
+    if not data.get("success"):
         return "❌ No Result"
 
-    results = data["results"]
+    results = data.get("result", [])
 
-    text = f"✅ {len(results)} Results Found:\n\n"
+    text = f"Response:\n✅ {len(results)} Results Found:\n\n"
 
     for i, res in enumerate(results, start=1):
-
-        address = str(res.get("address", "N/A")).replace("!", " ")
 
         text += f"🔹 Result {i}:\n"
         text += f"📱 Mobile: {res.get('mobile','N/A')}\n"
         text += f"👤 Name: {res.get('name','N/A')}\n"
         text += f"👴 Father's Name: {res.get('father_name','N/A')}\n"
-        text += f"🏠 Address: {address}\n"
+        text += f"🏠 Address: {res.get('address','N/A')}\n"
         text += f"🔴 Circle: {res.get('circle','N/A')}\n"
-        text += f"🆔 Aadhar/ID: {res.get('aadhar','N/A')}\n"
-        text += f"📞 Alternate Mobile: {res.get('alternate_mobile','N/A')}\n\n"
+        text += f"🆔 Aadhar/ID: {res.get('id_number','N/A')}\n"
+        text += f"📞 Alternate Mobile: {res.get('alt_mobile','N/A')}\n\n"
 
     text += "👤 Powered by @Mahakal1814"
 
